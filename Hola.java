35942d40f184e637f6679b867094d3e353938617
JorgeCsstro <145020014+JorgeCsstro@users.noreply.github.com>
2023-09-14 16:35:48 +0200

    Add files via upload

---
+++ Hola.java
@@ -0,0 +1,9 @@
+//package mipaquete;
+
+public class Hola
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hola mundo"); //Muestra en pantalla lo de entre parentesis
+	}
+}
---
+++ Test2.java
@@ -0,0 +1,19 @@
+public class Test2
+{
+	public static void main(String[] args)
+	{
+		float f = 100.5;
+		long l = f; //conversion automatica de tipo
+		int i = f; //conversion automatica de tipo
+		System.out.println("Valor Int "+i);
+		System.out.println("Valor Long "+l);
+		System.out.println("Valor Float "+f);
+		
+		
+	}
+	
+	
+	
+	
+	
+}
