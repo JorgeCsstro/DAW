f8f23ac46fef79ccdd32a21119f80396c1122932
JorgeCsstro <145020014+JorgeCsstro@users.noreply.github.com>
2023-09-15 16:54:39 +0200

    Add files via upload

---
+++ Ejercicio33.java
@@ -0,0 +1,19 @@
+public class Ejercicio33 {
+
+	public static void main (String[] args) {
+
+        char c1 = 'E';
+        char c2 = '5';
+        char c3 = '?';
+
+
+
+        double a = c1 + 1;
+
+            System.out.println(a);
+        
+        
+
+
+	}
+}
