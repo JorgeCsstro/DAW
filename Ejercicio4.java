3d5575a373fac5d9f2516be454aa8c6ca2bd6ea8
JorgeCsstro <145020014+JorgeCsstro@users.noreply.github.com>
2023-09-19 19:37:16 +0200

    Add files via upload

---
+++ Ejercicio4.java
@@ -0,0 +1,10 @@
+public class Ejercicio4 {
+
+	public static void main (String[] args) {
+
+        String nombre = "Jorge Castro Tramoyeres";
+
+            System.out.println("Buenos días, mi nombre es "+nombre);
+
+	}
+}
