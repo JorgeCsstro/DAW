32d0129365535fbbbf84ae8943b86921768b6d84
JorgeCsstro <145020014+JorgeCsstro@users.noreply.github.com>
2023-09-19 20:40:19 +0200

    2ª tarea

---
+++ Ejercicio6.java
@@ -0,0 +1,22 @@
+public class Ejercicio6 {
+
+	public static void main (String[] args) {
+
+        String nombre = "Jorge Castro Tramoyeres";
+
+            int index1 = nombre.indexOf(" ");// coje el index del espacio
+            int index2 = nombre.indexOf(" ", index1 +1);// coje el index del 2o espacio
+
+
+            String nomb = nombre.substring(0, index1);// coje a partir del index 0 (el inicio de la frase) hasta el 1er index, es decir, el primer espacio         
+            String ape1 = nombre.substring(index1+1, index2);// coje lo que esta entre el 1er espacio y el 2o espacio / se pone +1 para que NO COJA EL ESPACIO
+            String ape2 = nombre.substring(index2+1);// coje lo que esta a partir del 2o espacio
+
+            System.out.println(nomb.length());
+            System.out.println(ape1.length());            
+            System.out.println(ape2.length());
+
+        
+
+	}
+}
